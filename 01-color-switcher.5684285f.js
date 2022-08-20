cd442b95a250c9bff8359082127248fbfc38163b
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-08-20 09:29:59 +0000

    Deploying to gh-pages from @ SergeyKartavcev/goit-js-hw-09@dd716afd9f8499ce4e129d9058780f0a6172b220 🚀

--- 01-color-switcher.5684285f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(()=>{a.start()})),e.addEventListener("click",(()=>{a.stop()}));const a={intervalId:0,start(){this.isActive,this.intervalId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.removeAttribute("disabled")},stop(){clearInterval(this.intervalId),e.disabled=!0,t.removeAttribute("disabled")}};
-//# sourceMappingURL=01-color-switcher.5684285f.js.map
